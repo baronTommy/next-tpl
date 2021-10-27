91c2737c21fcf593f4c00607b171d46a003fe388
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-27 21:38:13 +0900

    fix: :bug: fix commit, update conf

--- interactive-commit.config.js
+++ interactive-commit.config.js
@@ -46,6 +46,7 @@ BREAKING CHANGE: {{breakingChange}}`,
      * This is the theme color of the terminal.
      */
     color: "green",
+    templateName: "Conventional Commit with gitmoji",
   },
   questionDictionary: [
     {
